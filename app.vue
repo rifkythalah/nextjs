77d948395b3f6f5e6dbd92627e69c5a0e5c7eea9
rifkythalah <athallahrifqi29@gmail.com>
2024-09-24 23:29:57 +0700

    homepage

---
+++ app.vue
@@ -0,0 +1,22 @@
+<!-- pages/index.vue -->
+<template>
+  <div>
+    <h1>Selamat Datang di Homepage!</h1>
+    <p>Ini adalah halaman utama dari aplikasi Nuxt.js Anda.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #2c3e50;
+}
+p {
+  font-size: 18px;
+}
+</style>
